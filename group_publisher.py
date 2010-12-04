392c0f4d296cf1f8fa194849268fe0150a509b8f
Gavin Carothers <gavin@carothers.name>
2010-12-04 13:00:24 -0800

    Now with option parser!

--- group_publisher.py
+++ group_publisher.py
@@ -5,11 +5,25 @@ from optparse import OptionParser
 
 Publisher = namedtuple('Publisher','prefix year name')
 
-def main(*args):
-    with open('publishers') as publishers_file:
-        publishers = read_publishers_set(publishers_file)
+def main():
+    usage = "usage: %prog [options] publisher_tuples"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-f", "--format", action="store", type="choice", 
+                      choices=["rdf", "text"], default="text", dest="format")
+    parser.add_option("-n", "--sample-size", action="store", type=int, 
+                      default=None, dest="sample_size")
+    (options, args) = parser.parse_args()
+    if len(args) != 1:
+        parser.error("Must specify publisher_tuples file")
+    publishers_file_name = args[0]
+    with open(publishers_file_name) as publishers_file:
+        publishers = read_publishers_set(publishers_file, 
+                                         options.sample_size)
     publishers_by_name = get_publishers_by_name(publishers)
-    output_text(publishers_by_name)
+    if options.format == 'rdf':
+        output_rdf(publishers_by_name)
+    elif options.format == 'text':
+        output_text(publishers_by_name)
     
         
 def get_publishers_by_name(publishers):
@@ -27,22 +41,56 @@ def as_sortname(publisher):
     sortname = sortname.strip()
     return sortname
         
-def read_publishers_set(publishers_file):
-    publishers = set() # Tons of duplictes
+def read_publishers_set(publishers_file, sample_size=None):
+    """Return a set from a file containing python tuples in the form:
+    (u'0220', 1996, u'Penguin')
+    (u'0669', 1990, u'D.C. Heath & Co')
+    Stop after reading sample_size number of tuples to nearest 10,000
+    """
+    publishers = set()
     for i, line in enumerate(publishers_file):
         pub = Publisher(*eval(line))
         publishers.add(pub)
         if i % 10000 == 0:
             print >> sys.stderr, "Processing:", i
-            if i == 5000000:
+            if sample_size and i >= sample_size:
                 break
+    print >> sys.stderr, "Processed:", i , "Unique Publishers:", len(publishers)
     return publishers
 
         
 def output_rdf(publishers_by_name):
-    pass
+    """Output RDF N3 for each publisher in the form:
+    pub:oreillymedia a foaf:Organization;
+    foaf:name "O' Reilly Media Inc."@en,
+        "O'Reilly Media"@en,
+        "O'Reilly Media Inc"@en,
+        "O'Reilly Media Inc."@en,
+        "O'Reilly Media, Inc."@en,
+        "O'ReillyMedia, Inc."@en,
+        "O'reilly Media"@en .
+    """
+    import pymantic.RDF
+    import rdflib
+    graph = rdflib.ConjunctiveGraph()
+    @pymantic.RDF.register_class('foaf:Organization')
+    class Organization(pymantic.RDF.Resource):
+        namespaces = {'foaf':'http://xmlns.com/foaf/0.1/'}        
+    for key in publishers_by_name:
+        uri = "http://gavin.carothers.name/work/archive/publishers/%s" % key
+        publisher = Organization.new(graph, uri)
+        pubs = publishers_by_name[key]
+        names = set()
+        for pub in pubs:
+            names.add(pub.name)
+        publisher['foaf:name'] = names
+    graph.bind('pub', rdflib.Namespace('http://gavin.carothers.name/work/archive/publishers/'))
+    graph.bind('foaf', rdflib.Namespace('http://xmlns.com/foaf/0.1/'))
+    print graph.serialize(format='n3')
     
 def output_text(publishers_by_name):
+    """Output plain text with Publisher tuples in the form:
+    """
     for key in sorted(publishers_by_name):
         pubs = publishers_by_name[key]
         print key, len(pubs)
@@ -50,4 +98,4 @@ def output_text(publishers_by_name):
             print "\t", pub
             
 if __name__ == "__main__":
-    sys.exit(main(sys.argv))
+    sys.exit(main())
